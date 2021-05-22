048e9b847286ef02bb34155b72eb0fee1b359c29
rockit-man <demetrius.esq@gmail.com>
2021-05-22 14:47:00 -0500

    JobField abstract class created and inherited by the job field classes, code is mostly DRY

--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -50,8 +50,6 @@ public class JobTest {
 
     @Test
     public void testsToStringForBlanksLines() {
-//        System.out.println(newJob.toString());
-//        System.out.println(newJob.toString().trim());
         assertFalse(newJob.toString().trim().isEmpty());
     }
 
@@ -64,42 +62,16 @@ public class JobTest {
                 "\nPosition Type: " + newJob.getPositionType() +
                 "\nCore Competency: " + newJob.getCoreCompetency() + "\n";
         assertEquals(newJob.toString(), newJobString);
-//        System.out.println(newJobString);
     }
 
     @Test
     public void testsIfFieldsAreEmpty() {
-//        Job job5 = new Job(null, null, null, null, null);
-
-//        System.out.println(job5);
-//        System.out.println(job5.toString());
-//        String job5AsString = job5.toString();
-//        System.out.println(job5AsString);
-//        job5AsString = job5AsString.replace("null", "Data Not Available");
-//        System.out.println(job5AsString);
-
-        System.out.println("job1:");
-        System.out.println(job1.toString());
-        System.out.println("job2:");
-        System.out.println(job2.toString());
-        System.out.println("job3:");
-        System.out.println(job3.toString());
-        System.out.println("job4:");
-        System.out.println(job4.toString());
-        System.out.println("newJob:");
-        System.out.println(newJob.toString());
-        System.out.println("job5:");
-        System.out.println(job5.toString());
-
         String job5Expected = "\nID: " + "18" +
                 "\nName: " + "Data not available" +
                 "\nEmployer: " + "Data not available" +
                 "\nLocation: " + "Data not available" +
                 "\nPosition Type: " + "Data not available" +
                 "\nCore Competency: " + "Data not available" + "\n";
-
-//        System.out.println(job5Expected);
-
         assertEquals(job5Expected, job5.toString());
     }
 
